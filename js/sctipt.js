145ec26e99cd306aca3f63839211e7f2b3629b44
Artsiom <example@gmail.com>
2021-03-28 16:41:04 +0300

    task_v1.2

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,32 +1,27 @@
-"use strict";
+
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-
-const lastFilmScore = +prompt("На сколько оцениете его?", "");
-
-const lastFilm1 = prompt("Один из последних просмотренных фильмов?", "");
-
-const lastFilmScore1 = +prompt("На сколько оцениете его?", "");
-
-const count = numberOfFilms;
-const movies = {
-   lastFilm,
-   lastFilmScore
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
 };
-const actors = {};
-const genres = [];
-const privat = false;
 
 
 
-const personalMovieDb = [ count, movies, actors, genres, privat];
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = +prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = +prompt("На сколько оцените его?", "")
+;
+ 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 
 
 
-
-console.log(`'${lastFilm}' : ${lastFilmScore}`);
-
-console.log(`'${lastFilm1}' : ${lastFilmScore1}`);
+console.log(personalMovieDB);
