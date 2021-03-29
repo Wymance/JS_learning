a2bcd59a4da757b028db96237d8a0b10401f1b63
Artsiom <example@gmail.com>
2021-03-29 20:07:35 +0300

    task2_1

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,7 +1,13 @@
 
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
+while (numberOfFilms == 0 || numberOfFilms == null || numberOfFilms.length >= 50) {   
+   numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+}
+
+console.log(numberOfFilms);
+ 
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {},
@@ -10,18 +16,26 @@ const personalMovieDB = {
    privat: false
 };
 
+if (personalMovieDB.count < 10) {
+   alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   alert('Вы классический зритель');
+} else if (personalMovieDB.count >30 ) {
+   alert('Вы киноман');
+} else {
+   alert('Произошла ошибка'); 
+}
 
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = +prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = +prompt("На сколько оцените его?", "")
-;
- 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-
+for (let i = 1; i < 3; i++) {
+   let a = prompt("Один из последних просмотренных фильмов?", "");
+   while (a == 0 || a == null || a.length >= 50) {
+       a = prompt("Один из последних просмотренных фильмов?", "");
+   }
+   let b = prompt("На сколько оцените его?", "");  
+   personalMovieDB.movies[a] = b;
+} 
 
 
 console.log(personalMovieDB);
